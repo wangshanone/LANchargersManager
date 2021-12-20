981f2db7e835461c01f266ac4b94de571e66bcd7
wangshanone <78083347+wangshanone@users.noreply.github.com>
2021-12-20 11:46:29 +0800

    Add files via upload

---
+++ TESTAPP/MyServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sodao.FastSocket.Server;
+using Sodao.FastSocket.Server.Messaging;
+using Sodao.FastSocket.SocketBase;
+
+namespace TESTAPP
+{
+   public class MyServer : AbsSocketService<CommandLineMessage>
+    {
+
+
+        public override void OnConnected(IConnection connection)
+        {
+            base.OnConnected(connection);
+            Console.WriteLine(connection.RemoteEndPoint.ToString() + " " + connection.ConnectionID.ToString() + " connected");
+            connection.BeginReceive();
+        }
+
+        public override void OnReceived(IConnection connection, CommandLineMessage message)
+        {
+
+           
+            base.OnReceived(connection, message);
+
+            message.Reply(connection, "GET DATA OK"); //此处最好用一部编程或者消息队列 （非常重要）
+
+        }
+
+        public override void OnDisconnected(IConnection connection, Exception ex)
+        {
+            base.OnDisconnected(connection, ex);
+            Console.WriteLine(connection.RemoteEndPoint.ToString() + " disconnected");
+        }
+
+        public override void OnException(IConnection connection, Exception ex)
+        {
+            base.OnException(connection, ex);
+            Console.WriteLine(ex.ToString());
+        }
+
+    }
+
+
+}
